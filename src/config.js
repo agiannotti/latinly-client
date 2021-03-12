66b2b82273148372c87e20b40d81656eae1aa151
tonyg <aj.gianno@gmail.com>
2021-03-11 20:56:21 -0600

    vercel

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://spaced-rep-ajg.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
-  REACT_APP_API_BASE: process.env.REACT_APP_API_BASE,
+  REACT_APP_API_BASE:
+    process.env.REACT_APP_API_BASE || 'http://localhost:8000/api',
 };
