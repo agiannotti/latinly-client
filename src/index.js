3a698ee495df1e0b29564cade29b4ffcca56b8fd
tonyg <aj.gianno@gmail.com>
2021-05-04 16:22:19 -0500

    merge and reduced

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import App from './components/App/App';
-import './setup-icons';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import { LanguageProvider } from './context/LanguageContext';
 import { LearnProvider } from './context/LearnContext';
 
@@ -22,5 +20,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
